ada640c4b79f2daf251199ef25341620e80a5886
huhaitao <huhaitao@100tal.com>
2019-07-22 21:02:08 +0800

    add dependencies

--- phaser/build/webpack.dev.js
+++ phaser/build/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+// TODO not work
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
